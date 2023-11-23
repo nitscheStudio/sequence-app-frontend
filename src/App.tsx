368dd80fc2e354d2129310e869884bd09919c660
nitscheStudio <151834833+nitscheStudio@users.noreply.github.com>
2023-11-23 11:38:08 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import SampleMusicPlayer from "./components/SampleMusicPlayer";
+
+function App() {
+  return (
+    <>
+      <SampleMusicPlayer />
+    </>
+  );
+}
+
+export default App;
