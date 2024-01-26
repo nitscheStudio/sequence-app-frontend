55b48b303e780b6ec9fa07bbbf2ba750f159d299
nitscheStudio <151834833+nitscheStudio@users.noreply.github.com>
2024-01-26 18:00:14 +0100

    start editSample

--- src/pages/EditSample.tsx
+++ src/pages/EditSample.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useForm } from "react-hook-form";
+import http from "../utils/http";
+import { useState } from "react";
+import { Sample } from "../types/sample";
 
-const EditSample = () => {
-  return (
-    <div>EditSample</div>
-  )
-}
+type EditSampleProps = {
+  sampleId: number;
+};
 
-export default EditSample
+const EditSample = ({ sampleId }: EditSampleProps) => {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
+
+  const [sampleData, setSampleData] = useState<Sample | null>(null);
+  //Fetch Sample from database
+  const getSampleInfo = async () => {
+    try {
+      await http.get("/sanctum/csrf-cookie");
+      const response = await http.get(`/sample/${sampleId}`);
+    } catch (error) {}
+  };
+
+  return <div>EditSample</div>;
+};
+
+export default EditSample;
